1d64107a724e30bf806697e27db9a67a7cf2e166
u724 <lgjy143@163.com>
2018-05-23 14:10:44 +0800

    module test

---
+++ VCore.Test/Modules/PlugInModuleLoading_Tests.cs
@@ -0,0 +1,72 @@
+﻿using Shouldly;
+using System.Linq;
+using VCore.Modules;
+using VCore.PlugIns;
+using Xunit;
+
+namespace VCore.Test.Modules
+{
+    public class PlugInModuleLoading_Tests : TestBaseWithLocalIocManager
+    {
+        [Fact]
+        public void Should_Load_All_Modules()
+        {
+            //Arrange
+            var bootstrapper = VcBootstrapper.Create<MyStartupModule>(options =>
+            {
+                options.IocManager = LocalIocManager;
+            });
+
+            bootstrapper.PlugInSources.AddTypeList(typeof(MyPlugInModule));
+
+            bootstrapper.Initialize();
+
+            //Act
+            var modules = bootstrapper.IocManager.Resolve<IVcModuleManager>().Modules;
+
+            //Assert
+            modules.Count.ShouldBe(6);
+
+            modules.Any(m => m.Type == typeof(VcKernelModule)).ShouldBeTrue();
+            modules.Any(m => m.Type == typeof(MyStartupModule)).ShouldBeTrue();
+            modules.Any(m => m.Type == typeof(MyModule1)).ShouldBeTrue();
+            modules.Any(m => m.Type == typeof(MyModule2)).ShouldBeTrue();
+            modules.Any(m => m.Type == typeof(MyPlugInModule)).ShouldBeTrue();
+            modules.Any(m => m.Type == typeof(MyPlugInDependedModule)).ShouldBeTrue();
+
+            modules.Any(m => m.Type == typeof(MyNotDependedModule)).ShouldBeFalse();
+        }
+
+        [DependsOn(typeof(MyModule1), typeof(MyModule2))]
+        public class MyStartupModule : VcModule
+        {
+
+        }
+
+        public class MyModule1 : VcModule
+        {
+
+        }
+
+        public class MyModule2 : VcModule
+        {
+
+        }
+
+        public class MyNotDependedModule : VcModule
+        {
+
+        }
+
+        [DependsOn(typeof(MyPlugInDependedModule))]
+        public class MyPlugInModule : VcModule
+        {
+
+        }
+
+        public class MyPlugInDependedModule : VcModule
+        {
+
+        }
+    }
+}
---
+++ VCore.Test/Modules/VcAssemblyFinder_Tests.cs
@@ -0,0 +1,42 @@
+﻿using Shouldly;
+using System.Linq;
+using System.Reflection;
+using VCore.Modules;
+using VCore.Reflection;
+using Xunit;
+
+namespace VCore.Test.Modules
+{
+    public class VcAssemblyFinder_Tests : TestBaseWithLocalIocManager
+    {
+        [Fact]
+        public void Should_Get_Module_And_Additional_Assemblies()
+        {
+            //Arrange
+            var bootstrapper = VcBootstrapper.Create<MyStartupModule>(options =>
+            {
+                options.IocManager = LocalIocManager;
+            });
+
+            bootstrapper.Initialize();
+
+            //Act
+            var assemblies = bootstrapper.IocManager.Resolve<VcAssemblyFinder>().GetAllAssemblies();
+
+            //Assert
+            assemblies.Count.ShouldBe(3);
+
+            assemblies.Any(a => a == typeof(MyStartupModule).GetAssembly()).ShouldBeTrue();
+            assemblies.Any(a => a == typeof(VcKernelModule).GetAssembly()).ShouldBeTrue();
+            assemblies.Any(a => a == typeof(FactAttribute).GetAssembly()).ShouldBeTrue();
+        }
+
+        public class MyStartupModule : VcModule
+        {
+            public override Assembly[] GetAdditionalAssemblies()
+            {
+                return new[] { typeof(FactAttribute).GetAssembly() };
+            }
+        }
+    }
+}
